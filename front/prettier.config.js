2dc4a5177a94d835b88df0d6044b27945e462129
kimhyunjo <alsxmqkqh23@gmail.com>
2023-08-17 00:43:46 +0900

    :wrench: Add prettier

---
+++ front/prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
+
+export default config;
